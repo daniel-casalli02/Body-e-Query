ea9a20258294403c654d8ffc2b8734cd6efeb24b
Daniel Casalli <danis.settk@gmail.com>
2025-08-29 11:32:36 -0300

    continuação

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import dados from "./src/data/dados.js"
+const {bruxos} = dados;
 // Criar aplicação com Express e configurar para aceitar JSON
 const app = express();
 app.use(express.json());
@@ -17,7 +19,59 @@ app.get("/", (req, res) => {
 
 
 // Aqui vão todas suas Rotas
+// Query Parameters no Node.js - API de Hogwarts
+app.get('/bruxos', (req, res) => {
+    const { casa, ano, especialidade, nome } = req.query;
+    let resultado = bruxos;
+  
+    if (casa) {
+      resultado = resultado.filter(b => b.casa.toLowerCase() === casa.toLowerCase());
+    }
+  
+    if (ano) {
+      resultado = resultado.filter(b => b.ano == ano);
+    }
+  
+    if (especialidade) {
+      resultado = resultado.filter(b => b.especialidade.toLowerCase().includes(especialidade.toLowerCase()));
+    }
+  
+    if (nome) {
+      resultado = resultado.filter(b => b.nome.toLowerCase().includes(nome.toLowerCase()));
+    }
+  
+    res.status(200).json({
+      total: resultado.length,
+      data: resultado
+    });
+});
 
+app.post('/bruxos', (req, res) =>{
+    const{ nome, casa, ano, varinha, mascote, patrono, especialidade, vivo } = req.body;
+    if(!nome || !casa || !ano || !vivo){
+        return res.status(400).json({
+            sucess: false,
+            massage:"nome, casa, ano e estar vivo são obrigatórios para um bruxo!"
+        });
+    }
+
+    const novoBruxo = {
+        id: bruxos.length + 1,
+        nome,
+        casa: casa,
+        ano: parseInt(ano),
+        varinha: varinha || "Ainda não definido",
+        mascote: mascote || "Ainda não definido",
+        especialidade: especialidade || "Ainda não definido",
+        vivo:vivo
+    }
+
+    bruxos.push(novoBruxo)
+    res.status(201).json({
+        success: true,
+        message: "Novo bruxo adicionado a Hogwarts!",
+        data: novoBruxo});
+});
 
 // Iniciar servidor escutando na porta definida
 app.listen(serverPort, () => {
